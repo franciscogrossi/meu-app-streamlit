ff7539e1d67df66f4be0a45e1c012db7359baa09
franciscogrossi <124843901+franciscogrossi@users.noreply.github.com>
2025-01-21 11:47:20 -0300

    Add files via upload

--- app.py
+++ app.py
@@ -1,12 +1,4 @@
 import os
-
-# Verifica se o Google Chrome já está instalado, senão instala
-if "google-chrome" not in os.popen("apt list --installed").read():
-    os.system("wget -q -O - https://dl.google.com/linux/linux_signing_key.pub | apt-key add -")
-    os.system("echo 'deb [arch=amd64] http://dl.google.com/linux/chrome/deb/ stable main' > /etc/apt/sources.list.d/google-chrome.list")
-    os.system("apt-get update")
-    os.system("apt-get install -y google-chrome-stable")
-
 import streamlit as st
 import time
 import pandas as pd
@@ -569,8 +561,9 @@ with st.container():
             url = f"https://fbref.com/en/players/{slug}/matchlogs/2024-2025/{nome_para_url}-Match-Logs"
 
             import chromedriver_autoinstaller
-            from selenium.webdriver.chrome.options import Options
             from selenium import webdriver
+            from selenium.webdriver.chrome.service import Service
+            from selenium.webdriver.chrome.options import Options
 
             # Instalar automaticamente o ChromeDriver
             chromedriver_autoinstaller.install()
@@ -580,7 +573,6 @@ with st.container():
             chrome_options.add_argument("--headless")  # Executar sem interface gráfica
             chrome_options.add_argument("--no-sandbox")  # Necessário em servidores remotos
             chrome_options.add_argument("--disable-dev-shm-usage")  # Evitar problemas de memória
-            chrome_options.binary_location = "/usr/bin/google-chrome-stable"  # Caminho do Chrome no Streamlit Cloud
 
             # Inicializar o driver com as opções configuradas
             driver = webdriver.Chrome(options=chrome_options)
