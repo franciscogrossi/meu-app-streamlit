01fe5bc2e02f188ac908ab7c71ba60cd272d8df9
franciscogrossi <124843901+franciscogrossi@users.noreply.github.com>
2025-01-28 14:21:40 -0300

    Update app.py

--- app.py
+++ app.py
@@ -729,7 +729,7 @@ footer {visibility: hidden;}
     position: relative;
     margin: 0;
     padding: 0;
-    font-size: 20em;  /* <-- Aumente conforme desejar */
+    font-size: 50em;  /* <-- Aumente conforme desejar */
     color: #2B2B2B;
 }
 
